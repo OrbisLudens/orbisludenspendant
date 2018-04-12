81c828aa5431aa25edd52bb6ab9fbb422aa1bfce
Martin Zehetmayer (Angrox) <angrox@idle.at>
2018-04-12 16:12:33 +0200

    added missing .h file

---
+++ orbisludenspendant.h
@@ -0,0 +1,19 @@
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <stdio.h>
+
+
+// ------------------- CONFIG SECTION ---------------------
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN            3
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      12
+
+// For a twelve LED Neopixel Ring we have an empty led after 2 Resonators. We mark them as "mod" (Mod slot)
+const char* resonatorleds[] = { "N", "NE", "mod", "E", "SE", "mod", "S", "SW", "mod", "W", "NW", "mod" };
+
+// Serial Console Speed
+const unsigned long BAUD_RATE = 115200;
+
