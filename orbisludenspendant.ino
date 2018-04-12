c8c8185dce6948bb3a220bc16150a33e34f36682
Martin Zehetmayer (Angrox) <angrox@idle.at>
2018-04-12 15:04:08 +0200

    Initial working version

--- orbisludenspendant.ino
+++ orbisludenspendant.ino
@@ -1,39 +1,8 @@
-#include <Adafruit_NeoPixel.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
-#include <stdbool.h>
-
-
-// ------------------- CONFIG SECTION ---------------------
-
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN            3
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      12
-
-// Serial Console Speed
-const unsigned long BAUD_RATE = 115200; 
-// WIFI SSID
-const char* ssid = "xxx";
-// WIFI Password 
-const char* password = "xxx";
-// URL to fetch
-const char* url = "http://5.45.98.140:8080/module/status/json";
-
-// ---------------- CONFIG SECTION END -------------------
-
+#include "orbisludenspendant.h"
+#include "wifi.h"
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-// For a twelve LED Neopixel Ring we have an empty led after 2 Resonators
-const char* resonatorleds[] = { "N", "NE", "", "E", "SE", "", "S", "SW", "", "W", "NW", "" };
-// Array with faction colors according to tecthulu module definition
-// 0 = grey(white), 1 = green, 2 = blue
-const uint32_t factioncolors[] = { pixels.Color(0, 0, 0), pixels.Color(0, 255, 0), pixels.Color(0, 0, 255) };
-
-
 
 void setup () {
 
@@ -56,37 +25,50 @@ void setup () {
 void loop() {
   String pos;
   String portalres[8];
- 
+  int reshealth[8];
+  int health;
+  
+  Serial.println("---");
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
  
     HTTPClient http;  //Declare an object of class HTTPClient
  
-    http.begin("http://5.45.98.140:8080/module/status/json");  //Specify request destination
-    int httpCode = http.GET();                                                                  //Send the request
+    http.begin(url);  //Specify request destination
+    int httpCode = http.GET();  //Send the request
  
     if (httpCode > 0) { //Check the returning code
  
       String payload = http.getString();   //Get the request response payload
-      Serial.println(payload);             //Print the response payload
-      StaticJsonBuffer<2000> JSONBuffer;
+      //Serial.println(payload);             //Print the response payload
+      StaticJsonBuffer<2000> JSONBuffer; // 2000 byte is the max length of a message
       JsonObject& tec_answer = JSONBuffer.parseObject(payload);
       
-      int faction = (int) tec_answer["status"]["controllingFaction"];
+      int faction = (int) tec_answer["status"]["controllingFaction"]; // Controlling faction. 0=none, 1=ENL, 2=RES
       Serial.println(faction);
       Serial.println(tec_answer["status"]["resonators"].as<String>());
-      int deployedresos = tec_answer["status"]["resonators"].size();
-      for (int i = 0; i < deployedresos; i++) {
-        pos = tec_answer["status"]["resonators"][i]["position"].as<String>();
-        portalres[i] = pos;
-        //Serial.println(pos);
-        //int posnr = findLedPosition(pos);
-        //Serial.println(posnr);
-      }
-      for (int i = 0; i < NUMPIXELS-1; i++) {
-        if (isvalueinarray(resonatorleds[i], portalres, 8)) {
+      int deployedresos = tec_answer["status"]["resonators"].size(); // Number of deployed resonators
+      Serial.printf("Deployed Resos: %d\n", deployedresos);
+      // Iterate over the json resonator subarray and get the health and the position
+      for (int i = 0; i < deployedresos; i++) { 
+        reshealth[i] = (int) tec_answer["status"]["resonators"][i]["health"];
+        portalres[i] = tec_answer["status"]["resonators"][i]["position"].as<String>();
+      } 
+      // Now iterate over the pixels and see if we have a resonator to show
+      for (int i = 0; i < NUMPIXELS; i++) {
+        Serial.printf("-------        Checking >> %s <<\n", resonatorleds[i]);
+        int pos = isvalueinarray((String)resonatorleds[i], portalres, 8);
+        if (pos != -1 ) {
           // light led with faction color
+          health = reshealth[pos];
+          Serial.printf("%d: faction color (%d health)\n", i, health);
+          int color[3] = {0, 0, 0};
+          color[faction]=health+20;
+          setPixel(i, color[0], color[1], color[2]);
+          
         } else {
-          // light led with white or clear led
+          // light led with white or clear led if the resonator is not captured
+          Serial.printf("%d: grey color!\n", i);
+          setPixel(i, 20, 20, 20);
         }
       }
     }
@@ -101,7 +83,7 @@ void loop() {
 
 // Find the LED according to the position
 int findLedPosition(String pos) {
-  for (int i = 0; i < sizeof(resonatorleds) -1; i++) {
+  for (int i = 0; i < sizeof(resonatorleds)-1; i++) {
     if ((String) resonatorleds[i] == pos) {
       return i;
     }
@@ -109,18 +91,22 @@ int findLedPosition(String pos) {
   return -1;
 }
 
-
-bool isvalueinarray(int val, int *arr, int size){
+// Search a value in an array and return the position
+int isvalueinarray(String val, String arr[], int size){
     int i;
-    for (i=0; i < size; i++) {
-        if (arr[i] == val)
-            return true;
+    for (i = 0; i < size; i++) {
+        if (val.equals(arr[i])) {
+            return i;
+        }
     }
-    return false;
+    return -1;
 }
 
-
-
+// Light up that pixel!
+void setPixel(int pos, int r, int g, int b) {
+  Serial.printf("Set Pixel %d to (%d, %d, %d)\n", pos, r, g, b);
+  pixels.setPixelColor(pos, r, g, b);
+}
 
 
 
